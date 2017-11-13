2c182d79b68764cb97b56b0ef9c0956716575223
Mike Solomon <msolomon@thrillist.com>
2017-11-13 11:15:26 -0500

    dont send empty body

--- app.js
+++ app.js
@@ -123,7 +123,8 @@ app.post('/', function(req, res) {
         if (!thisUser) {
           // send a 200 for the slash command
           res.setHeader('Content-Type', 'application/json');
-          res.status(200).send({})
+          res.setStatus(200)
+          res.send(JSON.stringify({ success: true }))
           // the user must auth with Jira first
           slack.sendPrivateMessage(req.body.channel_id,
                                    req.body.user_id,
@@ -134,7 +135,8 @@ app.post('/', function(req, res) {
             .then(success => {
               // slack will post OK in the channel if you just return 200
               res.setHeader('Content-Type', 'application/json');
-              res.status(200).send({})
+              res.setStatus(200)
+              res.send(JSON.stringify({ success: true }))
             })
             .catch(err => {
               res.sendStatus(500)
