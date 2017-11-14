34cc30377e74653deb14d9a1f2a6d598c7983d07
Mike Solomon <msolomon@thrillist.com>
2017-11-13 21:10:59 -0500

    try another lib from atlassian

--- jira/index.js
+++ jira/index.js
@@ -1,17 +1,27 @@
 const
   request = require('request'),
   APP_URL = process.env.APP_URL || 'http://localhost:5000/',
-  OAuth = require('mashape-oauth').OAuth;
+  OAuth = require('oauth').OAuth;
 
+var consumer =
+  new OAuth("https://nowthis.atlassian.net/plugins/servlet/oauth/request-token",
+                  "https://nowthis.atlassian.net/plugins/servlet/oauth/access-token",
+                  'neptune-the-doodle',
+                  "",
+                  "1.0",
+                  `${APP_URL}auth/atlassian-oauth/callback`,
+                  "RSA-SHA1",
+				          null,
+				          process.env.RSA_PRIVATE_KEY);
 
-var oa = new OAuth({
-  requestUrl: 'https://nowthis.atlassian.net/plugins/servlet/oauth/request-token',
-  accessUrl: 'https://nowthis.atlassian.net/plugins/servlet/oauth/access-token',
-  callback: `${APP_URL}auth/atlassian-oauth/callback`,
-  consumerKey: 'neptune-the-doodle',
-  consumerSecret: process.env.RSA_PRIVATE_KEY,
-  signatureMethod: 'RSA-SHA1'
-});
+// var oa = new OAuth({
+//   requestUrl: 'https://nowthis.atlassian.net/plugins/servlet/oauth/request-token',
+//   accessUrl: 'https://nowthis.atlassian.net/plugins/servlet/oauth/access-token',
+//   callback: `${APP_URL}auth/atlassian-oauth/callback`,
+//   consumerKey: 'neptune-the-doodle',
+//   consumerSecret: process.env.RSA_PRIVATE_KEY,
+//   signatureMethod: 'RSA-SHA1'
+// });
 
 
 // var oauth = new OAuth.OAuth(
@@ -31,16 +41,27 @@ var helpers = {
 var functions = {
   makeJiraRequest: function(user) {
     return new Promise(function(resolve, reject) {
+      console.log('user creds')
+      console.log(user)
+      consumer.get("https://nowthis.atlassian.net/rest/api/2/issue/37440",
+      						user.jiraToken,
+      						user.jiraTokenSecret,
+      						"application/json",
+      						function(error, data, resp){
+      							console.log(data);
+              			data = JSON.parse(data);
+              			//response.send("I am looking at: "+data["key"]);
+      						});
 
-      oa.get({
-        url: 'https://nowthis.atlassian.net/rest/api/2/issue/37440',
-        oauth_token: user.jiraToken,
-        oauth_token_secret: user.tokenSecret,
-      }, function(data) {
-        console.log('GOT DATA')
-        console.log(data)
-        return resolve(data)
-      });
+      // oa.get({
+      //   url: 'https://nowthis.atlassian.net/rest/api/2/issue/37440',
+      //   oauth_token: user.jiraToken,
+      //   oauth_token_secret: user.tokenSecret,
+      // }, function(data) {
+      //   console.log('GOT DATA')
+      //   console.log(data)
+      //   return resolve(data)
+      // });
 
       // oauth.get('https://nowthis.atlassian.net/rest/api/2/issue/37440',
       // user.jiraToken, //test user token
