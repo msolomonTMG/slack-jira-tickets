4e54e307790bba897d9fe8fecb732e59f8d6057d
Mike Solomon <msolomon@thrillist.com>
2017-11-17 10:43:42 +0100

    change message in slack

--- app.js
+++ app.js
@@ -184,9 +184,10 @@ app.post('/', function(req, res) {
                 let jiraURL = issue.self.split('/rest/api')[0];
 
                 slack.sendMessage(payload.channel.id,
-                  `:raised_hands: ${issue.fields.creator.displayName} created *<${jiraURL}/browse/${issue.key}|${issue.key}: ${issue.fields.summary}>*`,
+                  `:raised_hands: ${issue.fields.creator.displayName} created an issue with the \`/ticket\` command!`,
                   [{
                     fallback: `${issue.fields.creator.displayName} created <${jiraURL}/browse/${issue.key}|${issue.key}: ${issue.fields.summary}>`,
+                    title: `<${jiraURL}/browse/${issue.key}|${issue.key}: ${issue.fields.summary}>`,
                     color: 'good',
                     thumb_url: `${issue.fields.creator.avatarUrls["48x48"]}`,
                     fields: [{
