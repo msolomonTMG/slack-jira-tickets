b6afb66a9572414fe223fef7811fce2a03347f70
Mike Solomon <msolomon@thrillist.com>
2018-01-10 22:02:02 -0500

    var log

--- app.js
+++ app.js
@@ -165,6 +165,7 @@ app.post('/', function(req, res) {
   } else if (req.body.payload) {
 
     let payload = JSON.parse(req.body.payload)
+
     console.log(payload)
 
     if (payload.type == 'dialog_submission') {
