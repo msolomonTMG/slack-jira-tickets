e02bba4bb701ea4ac11d70bb1ef4c921d8ae548d
Mike Solomon <msolomon@thrillist.com>
2018-01-10 21:13:45 -0500

    stop loggin so much i cant read

--- jira/index.js
+++ jira/index.js
@@ -29,12 +29,6 @@ var helpers = {
         payload,
         'application/json',
         function(error, data, resp) {
-          console.log("---------ERROR---------")
-          console.log(error)
-          console.log("---------DATA---------")
-          console.log(data)
-          console.log("---------RESP---------")
-          console.log(resp)
           return resolve(data)
         })
     });
