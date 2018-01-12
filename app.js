0d771aca3c9b95b0b120d4daf6cdafd749352ec7
Mike Solomon <msolomon@thrillist.com>
2018-01-11 23:48:29 -0500

    dont wait for label go sync

--- app.js
+++ app.js
@@ -192,31 +192,29 @@ app.post('/', function(req, res) {
                   let boardId = selectedProject.boardId
                   console.log(boardId)
 
+                  // this errors out for some reason but still applies label
                   jira.labelIssue(thisUser, issue, 'interruption')
-                    .then(labelled => {
 
-                      jira.getActiveSprint(thisUser, boardId)
-                        .then(activeSprint => {
-                          console.log(activeSprint)
-                          jira.addIssueToActiveSprint(thisUser, issue, activeSprint)
-                            .then(success => {
-                              //send slack
-                              slack.sendMessage(payload.channel.id,
-                                `:bangbang: ${issue.fields.creator.displayName} created an issue with the \`/ticket\` command and added it to the current sprint!`,
-                                [{
-                                  fallback: `${issue.fields.creator.displayName} created <${jiraURL}/browse/${issue.key}|${issue.key}: ${issue.fields.summary}>`,
-                                  title: `<${jiraURL}/browse/${issue.key}|${issue.key}: ${issue.fields.summary}>`,
-                                  color: 'good',
-                                  thumb_url: `${issue.fields.creator.avatarUrls["48x48"]}`,
-                                  fields: [{
-                                    title: "Description",
-                                    value: `${issue.fields.description}`,
-                                    short: false
-                                  }]
-                                }]
-                              )
-
-                            })
+                  jira.getActiveSprint(thisUser, boardId)
+                    .then(activeSprint => {
+                      console.log(activeSprint)
+                      jira.addIssueToActiveSprint(thisUser, issue, activeSprint)
+                        .then(success => {
+                          //send slack
+                          slack.sendMessage(payload.channel.id,
+                            `:bangbang: ${issue.fields.creator.displayName} created an issue with the \`/ticket\` command and added it to the current sprint!`,
+                            [{
+                              fallback: `${issue.fields.creator.displayName} created <${jiraURL}/browse/${issue.key}|${issue.key}: ${issue.fields.summary}>`,
+                              title: `<${jiraURL}/browse/${issue.key}|${issue.key}: ${issue.fields.summary}>`,
+                              color: 'good',
+                              thumb_url: `${issue.fields.creator.avatarUrls["48x48"]}`,
+                              fields: [{
+                                title: "Description",
+                                value: `${issue.fields.description}`,
+                                short: false
+                              }]
+                            }]
+                          )
 
                         })
 
