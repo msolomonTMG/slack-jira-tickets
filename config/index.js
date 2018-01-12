1cfe2ccb47359395e946e1444f25151ef2946d6d
Mike Solomon <msolomon@thrillist.com>
2018-01-12 00:16:28 -0500

    add board numbers to config

--- config/index.js
+++ config/index.js
@@ -4,23 +4,23 @@ module.exports = {
     return [
       {
         name: "Ads",
-        key: "ADS"
+        key: "ADS",
+        boardId: "107"
       },
       {
         name: "Audience Insights",
-        key: "AI"
-      },
-      {
-        name: "Automated Testing",
-        key: "AT"
+        key: "AI",
+        boardId: "125"
       },
       {
         name: "Data Platform",
-        key: "DP"
+        key: "DP",
+        boardId: "127"
       },
       {
         name: "Emerging Platforms",
-        key: "EP"
+        key: "EP",
+        boardId: "108"
       },
       {
         name: "Operations",
@@ -29,19 +29,23 @@ module.exports = {
       },
       {
         name: "Platform",
-        key: "PLAT"
+        key: "PLAT",
+        boardId: "110"
       },
       {
         name: "Platform Services",
-        key: "PS"
+        key: "PS",
+        boardId: "128"
       },
       {
         name: "Sales Insights",
-        key: "SI"
+        key: "SI",
+        boardId: "126"
       },
       {
         name: "Storytelling",
-        key: "STRY"
+        key: "STRY",
+        boardId: "130"
       },
       {
         name: "Mike Test",
