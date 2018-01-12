2324e65d96577c6da142289273972ed4063e3d11
Mike Solomon <msolomon@thrillist.com>
2018-01-11 22:55:58 -0500

    remove logs

--- jira/index.js
+++ jira/index.js
@@ -29,12 +29,12 @@ var helpers = {
         payload,
         'application/json',
         function(error, data, resp) {
-          console.log("---------ERROR---------")
-          console.log(error)
-          console.log("---------DATA---------")
-          console.log(data)
-          console.log("---------RESP---------")
-          console.log(resp)
+          // console.log("---------ERROR---------")
+          // console.log(error)
+          // console.log("---------DATA---------")
+          // console.log(data)
+          // console.log("---------RESP---------")
+          // console.log(resp)
           return resolve(data)
         })
     });
