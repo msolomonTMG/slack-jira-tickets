42343e5fbf4ac46719379d45bf2d07da96d14b18
Mike Solomon <msolomon@thrillist.com>
2018-01-11 22:04:26 -0500

    create config for projects

---
+++ config/index.js
@@ -0,0 +1,56 @@
+
+var config = {
+  projects: function() {
+    return
+    [
+      {
+        name: "Ads",
+        key: "ADS"
+      },
+      {
+        name: "Audience Insights",
+        key: "AI"
+      },
+      {
+        name: "Automated Testing",
+        key: "AT"
+      },
+      {
+        name: "Data Platform",
+        key: "DP"
+      },
+      {
+        name: "Emerging Platforms",
+        key: "EP"
+      },
+      {
+        name: "Operations",
+        key: "GNOPS",
+        boardId: "123"
+      },
+      {
+        name: "Platform",
+        key: "PLAT"
+      },
+      {
+        name: "Platform Services",
+        key: "PS"
+      },
+      {
+        name: "Sales Insights",
+        key: "SI"
+      },
+      {
+        name: "Storytelling",
+        key: "STRY"
+      },
+      {
+        name: "Mike Test",
+        key: "MIKETEST",
+        boardId: "115"
+      }
+    ]
+  }
+}
+
+module.exports = config;
